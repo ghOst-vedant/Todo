68b70b64ba80c8ad301347acad19c1108afad6c1
vedant <71282147+ghOst-vedant@users.noreply.github.com>
2023-10-05 06:09:03 +0000

    Updated the alert message

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-const inputBox =document.getElementById("input-box");
-const listContainer =document.getElementById("list");
-
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list");
 
 // Adds the todo to the list
 function addTask() {
-    if (inputBox.value ==='') {
-        alert("Write something.");
-    }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
-        listContainer.append(li);
-        let span=document.createElement("span");
-        span.innerHTML="&#10008"
-        li.appendChild(span);
-    }
-    inputBox.value="";
-    saveDate();
+  if (inputBox.value === "") {
+    alert("Enter something.");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.append(li);
+    let span = document.createElement("span");
+    span.innerHTML = "&#10008";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveDate();
 }
 
-listContainer.addEventListener("click",function(e){
-    if (e.target.tagName ==="LI") {
-        e.target.classList.toggle("checked");
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
     }
-    else if(e.target.tagName==="SPAN"){
-        e.target.parentElement.remove();
-    }
-},false);
-function saveDate(){
-    localStorage.setItem("data",listContainer.innerHTML);
-} 
+  },
+  false
+);
+function saveDate() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
